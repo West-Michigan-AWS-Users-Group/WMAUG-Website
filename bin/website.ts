d8562813809b1d27791c3ed85621a9d360b830ad
tnielsen2 <trentnielsen84@gmail.com>
2024-07-18 08:36:49 -0400

    Add CDK stack, migrate files to /app

--- bin/website.ts
+++ bin/website.ts
@@ -5,4 +5,8 @@ import { WebsiteStack } from "../lib/website-stack";
 
 const app = new cdk.App();
 new WebsiteStack(app, "WebsiteStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
