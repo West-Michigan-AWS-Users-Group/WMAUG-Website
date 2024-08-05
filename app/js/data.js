015f3e76c94d29218049561a5f0c6c0cfbe1a5ed
tnielsen2 <trentnielsen84@gmail.com>
2024-08-05 10:12:47 -0400

    Add new site logo

--- app/js/data.js
+++ app/js/data.js
@@ -2,7 +2,7 @@ export const newsFeedEntries = [
   {
     link: "https://wmaug.org/",
     imageAlt: "Contributors Wanted",
-    imageSrc: "assets/newsfeed/page-logo.webp",
+    imageSrc: "assets/newsfeed/wmaug-logo-new.webp.webp",
     title: "Contributors Wanted!",
     subtitle:
       "We need help with various projects, including web development, cloud engineering and documentation/app management. Join our Discord for more details.",
