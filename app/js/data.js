0c3827b37357e06972cc8d9794a120abaaa89b0b
tnielsen2 <trentnielsen84@gmail.com>
2024-08-05 09:16:37 -0400

    Update site in accordance with UG branding requirements (font, color scheme), remove bookstack entry, replace with contributors wanted. Refactor previous event picture to crop mismatched color

--- app/js/data.js
+++ app/js/data.js
@@ -1,17 +1,19 @@
-
 export const newsFeedEntries = [
-    {
-        link: 'https://deva-docs.wmaug.org/',
-        imageAlt: 'BookStack Banner',
-        imageSrc: 'assets/newsfeed/bookstack-banner.webp',
-        title: 'BookStack Created!',
-        subtitle: 'a simple, self-hosted, easy-to-use platform for organising and storing information.'
-    },
-    {
-        link: 'https://www.meetup.com/west-michigan-aws-users-group/events/302162787/',
-        imageAlt: 'AWS Event Banner',
-        imageSrc: 'assets/newsfeed/20240724-event-banner.webp',
-        title: 'Real-world experience moving from a monolith to microservices using AWS',
-        subtitle: 'Wednesday, July 24, 2024 6:00 PM to 8:00 PM EDT @ Davenport University, Grand Rapids, MI'
-    }
+  {
+    link: "https://wmaug.org/",
+    imageAlt: "Contributors Wanted",
+    imageSrc: "assets/newsfeed/page-logo.webp",
+    title: "Contributors Wanted!",
+    subtitle:
+      "We need help with various projects, including web development, cloud engineering and documentation/app management. Join our Discord for more details.",
+  },
+  {
+    link: "https://www.meetup.com/west-michigan-aws-users-group/events/302162787/",
+    imageAlt: "AWS Event Banner",
+    imageSrc: "assets/newsfeed/20240724-event-banner.webp",
+    title:
+      "Real-world experience moving from a monolith to microservices using AWS",
+    subtitle:
+      "Wednesday, July 24, 2024 6:00 PM to 8:00 PM EDT @ Davenport University, Grand Rapids, MI",
+  },
 ];
--- app/js/index.js
+++ app/js/index.js
@@ -1,43 +1,41 @@
-
-import {newsFeedEntries} from './data.js';
+import { newsFeedEntries } from "./data.js";
 
 function populateNewsFeed() {
+  const newsfeedElement = document.getElementById("newsfeed");
 
-    const newsfeedElement = document.getElementById('newsfeed');
+  newsFeedEntries.forEach((entry) => {
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("banner");
+    imageElement.alt = entry.imageAlt;
+    imageElement.src = entry.imageSrc;
 
-    newsFeedEntries.forEach(entry => {
-        const imageElement = document.createElement('img');
-        imageElement.classList.add('banner');
-        imageElement.alt = entry.imageAlt;
-        imageElement.src = entry.imageSrc;
+    const titleElement = document.createElement("span");
+    titleElement.classList.add("title");
+    titleElement.textContent = entry.title;
 
-        const titleElement = document.createElement('span');
-        titleElement.classList.add('title');
-        titleElement.textContent = entry.title;
+    const subtitleElement = document.createElement("span");
+    subtitleElement.classList.add("subtitle");
+    subtitleElement.textContent = entry.subtitle;
 
-        const subtitleElement = document.createElement('span');
-        subtitleElement.classList.add('subtitle');
-        subtitleElement.textContent = entry.subtitle;
+    const detailsElement = document.createElement("div");
+    detailsElement.appendChild(titleElement);
+    detailsElement.appendChild(subtitleElement);
+    detailsElement.classList.add("entry-details");
 
-        const detailsElement = document.createElement('div');
-        detailsElement.appendChild(titleElement);
-        detailsElement.appendChild(subtitleElement);
-        detailsElement.classList.add('entry-details');
+    const anchorElement = document.createElement("a");
+    anchorElement.appendChild(imageElement);
+    anchorElement.appendChild(detailsElement);
+    anchorElement.href = entry.link;
+    anchorElement.target = "_blank";
 
-        const anchorElement = document.createElement('a');
-        anchorElement.appendChild(imageElement);
-        anchorElement.appendChild(detailsElement);
-        anchorElement.href = entry.link;
-        anchorElement.target = '_blank';
+    const containerElement = document.createElement("div");
+    containerElement.appendChild(anchorElement);
+    containerElement.classList.add("newsfeed-entry");
 
-        const containerElement = document.createElement('div');
-        containerElement.appendChild(anchorElement);
-        containerElement.classList.add('newsfeed-entry');
-
-        newsfeedElement.appendChild(containerElement);
-    });
+    newsfeedElement.appendChild(containerElement);
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    populateNewsFeed();
+document.addEventListener("DOMContentLoaded", function () {
+  populateNewsFeed();
 });
