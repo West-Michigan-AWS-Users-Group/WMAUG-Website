357d341c5f7ce8f73d19f24eecca273125c1fedc
Justin Wheeler <justin.wheeler@gfs.com>
2024-08-19 21:24:06 -0400

    Created isPast in data.js and updated Historical Updates section for prior events, removed unused, added scroll to top button, updated styles, added latest event, updated .gitignore

--- app/js/index.js
+++ app/js/index.js
@@ -1,41 +1,60 @@
-import { newsFeedEntries } from "./data.js";
+import { newsFeedEntries } from './data.js';
 
-function populateNewsFeed() {
-  const newsfeedElement = document.getElementById("newsfeed");
+function onScroll() {
+  const scrollCtrl = document.getElementById('scroll-ctrl');
+  scrollCtrl.style.display =
+      document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+          ? 'block' : 'none';
+}
 
-  newsFeedEntries.forEach((entry) => {
-    const imageElement = document.createElement("img");
-    imageElement.classList.add("banner");
-    imageElement.alt = entry.imageAlt;
-    imageElement.src = entry.imageSrc;
-
-    const titleElement = document.createElement("span");
-    titleElement.classList.add("title");
-    titleElement.textContent = entry.title;
-
-    const subtitleElement = document.createElement("span");
-    subtitleElement.classList.add("subtitle");
-    subtitleElement.textContent = entry.subtitle;
-
-    const detailsElement = document.createElement("div");
-    detailsElement.appendChild(titleElement);
-    detailsElement.appendChild(subtitleElement);
-    detailsElement.classList.add("entry-details");
-
-    const anchorElement = document.createElement("a");
-    anchorElement.appendChild(imageElement);
-    anchorElement.appendChild(detailsElement);
-    anchorElement.href = entry.link;
-    anchorElement.target = "_blank";
-
-    const containerElement = document.createElement("div");
-    containerElement.appendChild(anchorElement);
-    containerElement.classList.add("newsfeed-entry");
-
-    newsfeedElement.appendChild(containerElement);
+function scrollToTop() {
+  window.scrollTo({
+    behavior: 'smooth', top: 0
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  populateNewsFeed();
-});
+function populateNewsFeed() {
+  const newsfeed = document.getElementById('newsfeed');
+  const newsfeedPast = document.getElementById('newsfeed-past');
+
+  newsFeedEntries.forEach((entry) => {
+    const imageElement = document.createElement('img');
+    imageElement.classList.add('banner');
+    imageElement.alt = entry.imageAlt;
+    imageElement.src = entry.imageSrc;
+
+    const titleElement = document.createElement('span');
+    titleElement.classList.add('title');
+    titleElement.textContent = entry.title;
+
+    const subtitleElement = document.createElement('span');
+    subtitleElement.classList.add('subtitle');
+    subtitleElement.textContent = entry.subtitle;
+
+    const detailsElement = document.createElement('div');
+    detailsElement.appendChild(titleElement);
+    detailsElement.appendChild(subtitleElement);
+    detailsElement.classList.add('entry-details');
+
+    const anchorElement = document.createElement('a');
+    anchorElement.appendChild(imageElement);
+    anchorElement.appendChild(detailsElement);
+    anchorElement.classList.add('newsfeed-entry');
+    anchorElement.href = entry.link;
+    anchorElement.target = '_blank';
+
+    if(entry.isPast){
+      newsfeedPast.appendChild(anchorElement);
+    } else {
+      newsfeed.appendChild(anchorElement);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', populateNewsFeed);
+
+window.onscroll = onScroll;
+
+document
+.getElementById('scroll-ctrl')
+.addEventListener('click', scrollToTop);
